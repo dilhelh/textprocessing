61ba200abc2219d08f0832b1d3509b4d4e815642
KGusarov <konstantins.gusarovs@gmail.com>
2016-02-04 14:19:15 +0200

    Cleaning things up

--- language-detection-common/src/main/java/com/cybozu/labs/langdetect/util/LangProfile.java
+++ language-detection-common/src/main/java/com/cybozu/labs/langdetect/util/LangProfile.java
@@ -7,8 +7,9 @@ import java.util.Set;
 /**
  * {@link LangProfile} is a Language Profile Class.
  * Users don't use this class directly.
- * 
+ *
  * @author Nakatani Shuyo
+ * @author Konstantin Gusarov
  */
 public class LangProfile {
     private static final int MINIMUM_FREQ = 2;
@@ -18,20 +19,23 @@ public class LangProfile {
     public int[] n_words = new int[NGram.N_GRAM];
 
     /**
-     * Constructor for JSONIC 
+     * Constructor for JSONIC
      */
-    public LangProfile() {}
+    public LangProfile() {
+    }
 
     /**
      * Normal Constructor
+     *
      * @param name language name
      */
     public LangProfile(String name) {
         this.name = name;
     }
-    
+
     /**
      * Add n-gram to profile
+     *
      * @param gram
      */
     public void add(String gram) {
@@ -53,14 +57,14 @@ public class LangProfile {
         if (name == null) return;   // Illegal
         int threshold = n_words[0] / LESS_FREQ_RATIO;
         if (threshold < MINIMUM_FREQ) threshold = MINIMUM_FREQ;
-        
+
         Set<String> keys = freq.keySet();
         int roman = 0;
-        for(Iterator<String> i = keys.iterator(); i.hasNext(); ){
+        for (Iterator<String> i = keys.iterator(); i.hasNext(); ) {
             String key = i.next();
             int count = freq.get(key);
             if (count <= threshold) {
-                n_words[key.length()-1] -= count; 
+                n_words[key.length() - 1] -= count;
                 i.remove();
             } else {
                 if (key.matches("^[A-Za-z]$")) {
@@ -72,29 +76,30 @@ public class LangProfile {
         // roman check
         if (roman < n_words[0] / 3) {
             Set<String> keys2 = freq.keySet();
-            for(Iterator<String> i = keys2.iterator(); i.hasNext(); ){
+            for (Iterator<String> i = keys2.iterator(); i.hasNext(); ) {
                 String key = i.next();
                 if (key.matches(".*[A-Za-z].*")) {
-                    n_words[key.length()-1] -= freq.get(key); 
+                    n_words[key.length() - 1] -= freq.get(key);
                     i.remove();
                 }
             }
-            
+
         }
     }
 
     /**
      * Update the language profile with (fragmented) text.
      * Extract n-grams from text and add their frequency into the profile.
+     *
      * @param text (fragmented) text to extract n-grams
      */
     public void update(String text) {
         if (text == null) return;
-        text = NGram.normalize_vi(text);
+        text = NGram.normalizeVietnamese(text);
         NGram gram = new NGram();
-        for(int i=0; i<text.length(); ++i) {
+        for (int i = 0; i < text.length(); ++i) {
             gram.addChar(text.charAt(i));
-            for(int n=1; n<=NGram.N_GRAM; ++n) {
+            for (int n = 1; n <= NGram.N_GRAM; ++n) {
                 add(gram.get(n));
             }
         }
---
+++ language-detection-common/src/main/java/com/cybozu/labs/langdetect/util/NGram.java
@@ -0,0 +1,355 @@
+package com.cybozu.labs.langdetect.util;
+
+import com.google.common.collect.Maps;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Character.UnicodeBlock.*;
+
+/**
+ * Cut out N-gram from text.
+ * Users don't use this class directly.
+ *
+ * @author Nakatani Shuyo
+ * @author Konstantin Gusarov
+ */
+class NGram {
+    private static final String LATIN1_EXCLUDED = Messages.getString("NGram.LATIN1_EXCLUDE");
+    private static final HashMap<Character, Character> CJK_MAP = Maps.newHashMap();
+
+    /**
+     * CJK Kanji Normalization Mapping
+     */
+    private static final String[] CJK_CLASSES = {
+            Messages.getString("NGram.KANJI_1_0"),
+            Messages.getString("NGram.KANJI_1_2"),
+            Messages.getString("NGram.KANJI_1_4"),
+            Messages.getString("NGram.KANJI_1_8"),
+            Messages.getString("NGram.KANJI_1_11"),
+            Messages.getString("NGram.KANJI_1_12"),
+            Messages.getString("NGram.KANJI_1_13"),
+            Messages.getString("NGram.KANJI_1_14"),
+            Messages.getString("NGram.KANJI_1_16"),
+            Messages.getString("NGram.KANJI_1_18"),
+            Messages.getString("NGram.KANJI_1_22"),
+            Messages.getString("NGram.KANJI_1_27"),
+            Messages.getString("NGram.KANJI_1_29"),
+            Messages.getString("NGram.KANJI_1_31"),
+            Messages.getString("NGram.KANJI_1_35"),
+            Messages.getString("NGram.KANJI_2_0"),
+            Messages.getString("NGram.KANJI_2_1"),
+            Messages.getString("NGram.KANJI_2_4"),
+            Messages.getString("NGram.KANJI_2_9"),
+            Messages.getString("NGram.KANJI_2_10"),
+            Messages.getString("NGram.KANJI_2_11"),
+            Messages.getString("NGram.KANJI_2_12"),
+            Messages.getString("NGram.KANJI_2_13"),
+            Messages.getString("NGram.KANJI_2_15"),
+            Messages.getString("NGram.KANJI_2_16"),
+            Messages.getString("NGram.KANJI_2_18"),
+            Messages.getString("NGram.KANJI_2_21"),
+            Messages.getString("NGram.KANJI_2_22"),
+            Messages.getString("NGram.KANJI_2_23"),
+            Messages.getString("NGram.KANJI_2_28"),
+            Messages.getString("NGram.KANJI_2_29"),
+            Messages.getString("NGram.KANJI_2_30"),
+            Messages.getString("NGram.KANJI_2_31"),
+            Messages.getString("NGram.KANJI_2_32"),
+            Messages.getString("NGram.KANJI_2_35"),
+            Messages.getString("NGram.KANJI_2_36"),
+            Messages.getString("NGram.KANJI_2_37"),
+            Messages.getString("NGram.KANJI_2_38"),
+            Messages.getString("NGram.KANJI_3_1"),
+            Messages.getString("NGram.KANJI_3_2"),
+            Messages.getString("NGram.KANJI_3_3"),
+            Messages.getString("NGram.KANJI_3_4"),
+            Messages.getString("NGram.KANJI_3_5"),
+            Messages.getString("NGram.KANJI_3_8"),
+            Messages.getString("NGram.KANJI_3_9"),
+            Messages.getString("NGram.KANJI_3_11"),
+            Messages.getString("NGram.KANJI_3_12"),
+            Messages.getString("NGram.KANJI_3_13"),
+            Messages.getString("NGram.KANJI_3_15"),
+            Messages.getString("NGram.KANJI_3_16"),
+            Messages.getString("NGram.KANJI_3_18"),
+            Messages.getString("NGram.KANJI_3_19"),
+            Messages.getString("NGram.KANJI_3_22"),
+            Messages.getString("NGram.KANJI_3_23"),
+            Messages.getString("NGram.KANJI_3_27"),
+            Messages.getString("NGram.KANJI_3_29"),
+            Messages.getString("NGram.KANJI_3_30"),
+            Messages.getString("NGram.KANJI_3_31"),
+            Messages.getString("NGram.KANJI_3_32"),
+            Messages.getString("NGram.KANJI_3_35"),
+            Messages.getString("NGram.KANJI_3_36"),
+            Messages.getString("NGram.KANJI_3_37"),
+            Messages.getString("NGram.KANJI_3_38"),
+            Messages.getString("NGram.KANJI_4_0"),
+            Messages.getString("NGram.KANJI_4_9"),
+            Messages.getString("NGram.KANJI_4_10"),
+            Messages.getString("NGram.KANJI_4_16"),
+            Messages.getString("NGram.KANJI_4_17"),
+            Messages.getString("NGram.KANJI_4_18"),
+            Messages.getString("NGram.KANJI_4_22"),
+            Messages.getString("NGram.KANJI_4_24"),
+            Messages.getString("NGram.KANJI_4_28"),
+            Messages.getString("NGram.KANJI_4_34"),
+            Messages.getString("NGram.KANJI_4_39"),
+            Messages.getString("NGram.KANJI_5_10"),
+            Messages.getString("NGram.KANJI_5_11"),
+            Messages.getString("NGram.KANJI_5_12"),
+            Messages.getString("NGram.KANJI_5_13"),
+            Messages.getString("NGram.KANJI_5_14"),
+            Messages.getString("NGram.KANJI_5_18"),
+            Messages.getString("NGram.KANJI_5_26"),
+            Messages.getString("NGram.KANJI_5_29"),
+            Messages.getString("NGram.KANJI_5_34"),
+            Messages.getString("NGram.KANJI_5_39"),
+            Messages.getString("NGram.KANJI_6_0"),
+            Messages.getString("NGram.KANJI_6_3"),
+            Messages.getString("NGram.KANJI_6_9"),
+            Messages.getString("NGram.KANJI_6_10"),
+            Messages.getString("NGram.KANJI_6_11"),
+            Messages.getString("NGram.KANJI_6_12"),
+            Messages.getString("NGram.KANJI_6_16"),
+            Messages.getString("NGram.KANJI_6_18"),
+            Messages.getString("NGram.KANJI_6_20"),
+            Messages.getString("NGram.KANJI_6_21"),
+            Messages.getString("NGram.KANJI_6_22"),
+            Messages.getString("NGram.KANJI_6_23"),
+            Messages.getString("NGram.KANJI_6_25"),
+            Messages.getString("NGram.KANJI_6_28"),
+            Messages.getString("NGram.KANJI_6_29"),
+            Messages.getString("NGram.KANJI_6_30"),
+            Messages.getString("NGram.KANJI_6_32"),
+            Messages.getString("NGram.KANJI_6_34"),
+            Messages.getString("NGram.KANJI_6_35"),
+            Messages.getString("NGram.KANJI_6_37"),
+            Messages.getString("NGram.KANJI_6_39"),
+            Messages.getString("NGram.KANJI_7_0"),
+            Messages.getString("NGram.KANJI_7_3"),
+            Messages.getString("NGram.KANJI_7_6"),
+            Messages.getString("NGram.KANJI_7_7"),
+            Messages.getString("NGram.KANJI_7_9"),
+            Messages.getString("NGram.KANJI_7_11"),
+            Messages.getString("NGram.KANJI_7_12"),
+            Messages.getString("NGram.KANJI_7_13"),
+            Messages.getString("NGram.KANJI_7_16"),
+            Messages.getString("NGram.KANJI_7_18"),
+            Messages.getString("NGram.KANJI_7_19"),
+            Messages.getString("NGram.KANJI_7_20"),
+            Messages.getString("NGram.KANJI_7_21"),
+            Messages.getString("NGram.KANJI_7_23"),
+            Messages.getString("NGram.KANJI_7_25"),
+            Messages.getString("NGram.KANJI_7_28"),
+            Messages.getString("NGram.KANJI_7_29"),
+            Messages.getString("NGram.KANJI_7_32"),
+            Messages.getString("NGram.KANJI_7_33"),
+            Messages.getString("NGram.KANJI_7_35"),
+            Messages.getString("NGram.KANJI_7_37"),
+    };
+
+    private static final String[] NORMALIZED_VI_CHARS = {
+            Messages.getString("NORMALIZED_VI_CHARS_0300"),
+            Messages.getString("NORMALIZED_VI_CHARS_0301"),
+            Messages.getString("NORMALIZED_VI_CHARS_0303"),
+            Messages.getString("NORMALIZED_VI_CHARS_0309"),
+            Messages.getString("NORMALIZED_VI_CHARS_0323")};
+
+    private static final String TO_NORMALIZE_VI_CHARS = Messages.getString("TO_NORMALIZE_VI_CHARS");
+    private static final String DMARK_CLASS = Messages.getString("DMARK_CLASS");
+    private static final Pattern ALPHABET_WITH_DMARK = Pattern.compile("([" + TO_NORMALIZE_VI_CHARS + "])(["
+            + DMARK_CLASS + "])");
+
+    static final int N_GRAM = 3;
+
+    static {
+        for (final String cjkClass : CJK_CLASSES) {
+            final char representative = cjkClass.charAt(0);
+
+            cjkClass.chars()
+                    .mapToObj(i -> (char) i)
+                    .forEach(c -> CJK_MAP.put(c, representative));
+        }
+    }
+
+    private String chars;
+    private boolean capital;
+
+    NGram() {
+        chars = " ";
+        capital = false;
+    }
+
+    /**
+     * Append a character into ngram buffer.
+     *
+     * @param ch    Character to add to buffer
+     */
+    void addChar(final char ch) {
+        final char normalized = normalize(ch);
+        final char lastchar = chars.charAt(chars.length() - 1);
+
+        if (lastchar == ' ') {
+            chars = " ";
+            capital = false;
+
+            if (normalized == ' ') {
+                return;
+            }
+        } else if (chars.length() >= N_GRAM) {
+            chars = chars.substring(1);
+        }
+
+        chars += normalized;
+
+        if (Character.isUpperCase(normalized)) {
+            if (Character.isUpperCase(lastchar)) {
+                capital = true;
+            }
+        } else {
+            capital = false;
+        }
+    }
+
+    /**
+     * Get n-Gram
+     *
+     * @param n     Length of n-gram
+     * @return      n-Gram String (null if it is invalid)
+     */
+    @Nullable
+    String get(final int n) {
+        if (capital) {
+            return null;
+        }
+
+        final int len = chars.length();
+        if (n < 1 || n > N_GRAM || len < n) {
+            return null;
+        }
+
+        if (n == 1) {
+            final char ch = chars.charAt(len - 1);
+            if (ch == ' ') {
+                return null;
+            }
+
+            return Character.toString(ch);
+        }
+
+        return chars.substring(len - n, len);
+    }
+
+    /**
+     * Character Normalization
+     *
+     * @param ch    Character to be normalized
+     * @return      Normalized character
+     */
+    static char normalize(final char ch) {
+        final Character.UnicodeBlock block = of(ch);
+        if (Objects.equals(block, GENERAL_PUNCTUATION)) {
+            return ' ';
+        }
+
+        if (Objects.equals(block, BASIC_LATIN)) {
+            if (ch < 'A' || (ch < 'a' && ch > 'Z') || ch > 'z') {
+                return ' ';
+            }
+        }
+
+        if (Objects.equals(block, LATIN_1_SUPPLEMENT)) {
+            if (LATIN1_EXCLUDED.indexOf(ch) >= 0) {
+                return ' ';
+            }
+        }
+
+        if (Objects.equals(block, LATIN_EXTENDED_B)) {
+            // normalization for Romanian
+            if (ch == '\u0219') {
+                // Small S with comma below => with cedilla
+                return '\u015f';
+            }
+
+            if (ch == '\u021b') {
+                // Small T with comma below => with cedilla
+                return '\u0163';
+            }
+        }
+
+        if (Objects.equals(block, ARABIC)) {
+            if (ch == '\u06cc') {
+                // Farsi yeh => Arabic yeh
+                return '\u064a';
+            }
+        }
+
+        if (Objects.equals(block, LATIN_EXTENDED_ADDITIONAL)) {
+            if (ch >= '\u1ea0') {
+                return '\u1ec3';
+            }
+        }
+
+        if (Objects.equals(block, HIRAGANA)) {
+            return '\u3042';
+        }
+
+        if (Objects.equals(block, KATAKANA)) {
+            return '\u30a2';
+        }
+
+        if (Objects.equals(block, BOPOMOFO) || Objects.equals(block, BOPOMOFO_EXTENDED)) {
+            return '\u3105';
+        }
+
+        if (Objects.equals(block, CJK_UNIFIED_IDEOGRAPHS)) {
+            if (CJK_MAP.containsKey(ch)) {
+                return CJK_MAP.get(ch);
+            }
+        }
+
+        if (Objects.equals(block, HANGUL_SYLLABLES)) {
+            return '\uac00';
+        }
+
+        return ch;
+    }
+
+    /**
+     * Normalizer for Vietnamese.
+     * Normalize Alphabet + Diacritical Mark(U+03xx) into U+1Exx .
+     *
+     * @param text      Vietnamese text to be normalized
+     * @return          Normalized text
+     */
+    static String normalizeVietnamese(final String text) {
+        final Matcher m = ALPHABET_WITH_DMARK.matcher(text);
+        final StringBuffer sb = new StringBuffer();
+
+        while (m.find()) {
+            final String a = m.group(1);
+            final int alphabet = TO_NORMALIZE_VI_CHARS.indexOf(a);
+
+            // Diacritical Mark
+            final String dm = m.group(2);
+            final int dmark = DMARK_CLASS.indexOf(dm);
+
+            final String replacement = NORMALIZED_VI_CHARS[dmark]
+                    .substring(alphabet, alphabet + 1);
+
+            m.appendReplacement(sb, replacement);
+        }
+
+        if (sb.length() == 0) {
+            return text;
+        }
+
+        m.appendTail(sb);
+        return sb.toString();
+    }
+}
